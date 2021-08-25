85b68c5667e1dc6a19d57ca6fd5dee6a970e7381
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-08-25 02:22:28 -0400

    commit tree 82c20af528d0d8f201351233ccb2426a93ce8e01

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,17 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species] 
+    #or 
+    # render json: birds, except: [:created_at, :updated_at] 
+  end
+
+  def show 
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird.slice(:id, :name, :species) 
+    else 
+      render json: { message: 'Bird not found' }
+    end 
   end
 end
